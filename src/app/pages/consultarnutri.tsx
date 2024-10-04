994d6aadfd114dfbf09e0196bbd1ac2112a212eb
Gabriel Henrique <henriquedeveloper24@gmail.com>
2024-10-04 16:40:52 -0300

    feat: inicio do projeto

---
+++ src/app/pages/consultarnutri.tsx
@@ -0,0 +1,10 @@
+
+
+export function ConsultarNutrientes() {
+    return (
+        <div>
+        <h1 className="font-bold text-3xl">Consultar Nutrientes</h1>
+        <input type="text" name="codebar" id="codebar" />
+        </div>
+    );
+}
